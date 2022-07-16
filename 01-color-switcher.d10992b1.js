0ab89a1c6b7618fca6659be9d91f2c49988c14b4
Vyacheslav <47851511+zerkel1991@users.noreply.github.com>
2022-07-16 11:24:21 +0300

    Initialize gh-pages

---
+++ 01-color-switcher.d10992b1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop"),d=null;t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(d)})),d=setInterval((()=>{e.disabled&&(document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`)}),1e3);
+//# sourceMappingURL=01-color-switcher.d10992b1.js.map
---
+++ 01-color-switcher.d731414f.js
@@ -0,0 +1,2 @@
+!function(){var e,t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop");d.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){t.disabled=!1,d.disabled=!0,clearInterval(e)})),e=setInterval((function(){t.disabled&&(document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)}();
+//# sourceMappingURL=01-color-switcher.d731414f.js.map
